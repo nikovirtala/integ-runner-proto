322c7a0b1c735869eeae1590f064322e5e37bf0f
Niko Virtala <niko.virtala@hey.com>
2023-02-28 08:19:33 +0200

    chore: add integ-tests and integ-runner

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,19 +1,49 @@
-import { awscdk } from "projen";
+import { awscdk, DependencyType, JsonFile } from "projen";
+
+const cdkVersion = "2.66.1";
+
 const project = new awscdk.AwsCdkTypeScriptApp({
   authorEmail: "niko.virtala@hey.com",
   authorName: "Niko Virtala",
   authorUrl: "https://nikovirtala.io",
-  cdkVersion: "2.66.1",
+  cdkVersion: cdkVersion,
   defaultReleaseBranch: "main",
   license: "MIT",
   minNodeVersion: "18.13.0",
   name: "integ-runner-proto",
   prettier: true,
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
+
+// integration test with `integ-runner` and `integ-tests`
+project.deps.addDependency(
+  `@aws-cdk/integ-runner@^${cdkVersion}`,
+  DependencyType.DEVENV
+);
+project.deps.addDependency(
+  `@aws-cdk/integ-tests-alpha@^${cdkVersion}-alpha.0`,
+  DependencyType.DEVENV
+);
+
+const integSnapshotTask = project.addTask("integ", {
+  description: "Run integration snapshot tests",
+  receiveArgs: true,
+  exec: "integ-runner --language typescript",
+});
+
+project.addTask("integ:update", {
+  description: "Run and update integration snapshot tests",
+  exec: "integ-runner --language typescript --update-on-failed",
+  receiveArgs: true,
+});
+
+new JsonFile(project, "integ.config.json", {
+  obj: {
+    maxWorkers: 10,
+    parallelRegions: ["eu-west-1"],
+  },
+  marker: false,
+});
+
+project.testTask.spawn(integSnapshotTask);
 project.synth();
